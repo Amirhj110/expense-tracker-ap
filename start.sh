324436d63f42049f61421736331a5c9bdf5aeeec
Amir <128501973+Amirhj110@users.noreply.github.com>
2026-03-29 17:59:41 +0500

    Update and rename start.sh to build.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python manage.py migrate --noinput
-python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
-python manage.py create_categories || true
-gunicorn expensetracker.wsgi:application
